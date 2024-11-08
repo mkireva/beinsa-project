0a35526b1491f66a05baa197256e29ddae6dba4b
mkireva <mariya@kireva.de>
2024-11-08 19:49:59 +0100

    fix:home

--- src/app/(rs)/home/page.tsx
+++ src/app/(rs)/home/page.tsx
@@ -6,7 +6,8 @@ export const metadata = {
     return (
       <div className="flex flex-col items-center justify-center gap-4 py-20">
         <h1 className="text-4xl font-bold">Home</h1>
-        <p className="text-lg">This is the home page.</p>
+      <div>Erste Vorträge </div>
+      <div>Sonntagsvorträge</div>
       </div>
     );
   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="bg-black bg-home-img bg-cover bg-center">
       <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
-        <div className="flex flex-col gap-6 p-12 items-center rounded-xl bg-sky-700/80 w-2/4 sm:max-w-1/3 mx-auto text-white sm:text-2xl">
+        <div className="flex flex-col gap-6 p-12 items-center rounded-xl bg-sky-700/80 w-3/4 sm:max-w-3/4 mx-auto text-white sm:text-2xl">
           <h1 className="text-4xl font-bold">Beinsa Projekt</h1>
           <p className="text-md">Vorträge und mehr von Beinsa Duno</p>
           <Link href="./home" >
