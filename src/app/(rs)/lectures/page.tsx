16d14166941cf91d366bc60ac1ba65dd31b2fbd5
mkireva <mariya@kireva.de>
2024-11-11 15:28:02 +0100

    fix: lectures

--- src/app/(rs)/lectures/page.tsx
+++ src/app/(rs)/lectures/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import ScrollText from "./ScrollText";
+import { cn } from "@/lib/utils";
 
 export const metadata = {
   title: "Vorträge",
@@ -10,13 +11,23 @@ export default function Lectures() {
     <div className="flex flex-col items-center justify-center py-10">
       <div className="w-full rounded-lg items-center justify-center">
         <div className="w-auto h-96 relative ">
-          <Image
-            loading={"lazy"}
-            fill
-            src={"/images/salona.jpg"}
-            alt={"Beisa Duno"}
-            className="rounded-lg object-cover transition-transform duration-500 group-hover:scale-110"
-          />
+          <div
+            className={cn(
+              "group relative overflow-hidden rounded-lg h-[400px]",
+              "shadow-lg transition-all duration-500",
+              "md:col-start-2"
+            )}
+          >
+            <div className="absolute inset-0">
+              <Image
+                loading={"lazy"}
+                fill
+                src={"/images/salona.jpg"}
+                alt={"Beisa Duno"}
+                className="rounded-lg object-cover transition-transform duration-500 group-hover:scale-110"
+              />{" "}
+            </div>{" "}
+          </div>
         </div>
       </div>
 
