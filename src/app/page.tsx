d47a15ca30422c6d86f4abaeb9ec4408be47cbdb
mkireva <mariya@kireva.de>
2024-11-13 21:10:55 +0100

    fix: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Ghost, LogIn } from "lucide-react";
+import { LogIn } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
