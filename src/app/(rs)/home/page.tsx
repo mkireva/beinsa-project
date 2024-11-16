3400cf2e11d375576fc6dc7afd7e321820bf5356
mkireva <mariya@kireva.de>
2024-11-16 19:33:18 +0100

    fix: home

--- src/app/(rs)/home/page.tsx
+++ src/app/(rs)/home/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 py-20">
       <div>
-        <Board />
+        {/* <Board /> */}
       </div>
     </div>
   );
