6b98d05c7b56a28db55408698f5c913973d47919
mkireva <mariya@kireva.de>
2024-11-18 19:55:17 +0100

    add: metadata

--- src/app/(rs)/layout.tsx
+++ src/app/(rs)/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer";
-import { Header } from "@/components/Header"
+import { Header } from "@/components/Header";
+import { Metadata } from "next";
 
 export default function RSLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,8 +8,34 @@ export default function RSLayout({ children }: { children: React.ReactNode }) {
       <div className="mx-auto w-full max-w-7xl">
         <Header />
         <div className="px-4 py-2">{children}</div>
-        <Footer/>
+        <Footer />
       </div>
     </>
   );
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    metadataBase: new URL("https://beins.de"),
+    title: "Beinsa Project",
+    description:
+      "Auf der Webseite finden Sie Vörträge, Bücher und Musik des spirituellen Lehrers Petar Danov.",
+    openGraph: {
+      title: "Beinsa Project",
+      description:
+        "Auf der Webseite finden Sie Vörträge, Bücher und Musik des spirituellen Lehrers Petar Danov.",
+      type: "website",
+      url: "https://beinsa.de",
+      siteName: "Beinsa Project",
+      images: "opengraph-image.png",
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@beinsa",
+      title: "Beinsa Project",
+      description:
+        "Auf der Webseite finden Sie Vörträge, Bücher und Musik vom spirituellen Lehrer Petar Danov.",
+      images: "opengraph-image.png",
+    },
+  };
 }
