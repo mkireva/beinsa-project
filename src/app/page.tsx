a0d72e589e53426f66177c2ade9926221bf5906a
mkireva <mariya@kireva.de>
2025-03-17 17:04:58 +0100

    fix: footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,11 @@ export default function Home() {
         <div className="flex flex-col gap-6 p-12 items-center rounded-xl bg-sky-700/80 w-2/3 sm:max-w-3/4 mx-auto text-white sm:text-2xl">
           <h1 className="text-4xl font-bold">Beinsa Projekt</h1>
           <p className="text-md">Vorträge und mehr von Beinsa Duno</p>
-          <p className="text-md">Die Seite befindet sich noch in der Entwicklung.</p>
-          <Link href="./home" >
-          <Button className="text-xl " variant="ghost"><LogIn className="w-22 h-22" />Enter</Button>
+          <Link href="./home">
+            <Button className="text-xl" variant="ghost">
+              <LogIn className="w-22 h-22" />
+              Enter
+            </Button>
           </Link>
         </div>
       </main>
