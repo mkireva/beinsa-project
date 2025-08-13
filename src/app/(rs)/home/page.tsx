c3d8d99d155601f9f1da51399292c1b5f592d017
mkireva <mariya@kireva.de>
2025-08-13 18:56:56 +0200

    update: home

--- src/app/(rs)/home/page.tsx
+++ src/app/(rs)/home/page.tsx
@@ -51,143 +51,60 @@ const booksByGenre = {
     },
     { title: "Das Gesetz des Dienens", year: { year: 1923, month: 4, day: 30 } },
     { title: "Die Wichtigkeit der kleinen Dinge", year: { year: 1924, month: 5, day: 31 } },
-    {title: "Friede sei mit Euch!", year: { year: 1925, month: 6, day: 1 },},
-    {title: "Die Notwendigkeit, Gott zu erkennen", year: { year: 1925, month: 6, day: 1 },},
-    {title: "Wie viel mehr ist nun ein Mensch als ein Schaf!", year: { year: 1925, month: 6, day: 1 },},
-    {title: "Pharisäer und Zöllner", year: { year: 1925, month: 6, day: 1 },},
-    {title: "Die Bedingungen für das ewige Leben", year: { year: 1925, month: 6, day: 1 },},
-    {title: "Die Furcht", year: { year: 1925, month: 6, day: 1 },},
-    {title: "Im Anfang war", year: { year: 1925, month: 6, day: 1 },},
-    {title: "Vernünftige Milch", year: { year: 1925, month: 6, day: 1 },},
+    { title: "Friede sei mit Euch!", year: { year: 1925, month: 6, day: 1 }, },
+    { title: "Die Notwendigkeit, Gott zu erkennen", year: { year: 1925, month: 6, day: 1 }, },
+    { title: "Wie viel mehr ist nun ein Mensch als ein Schaf!", year: { year: 1925, month: 6, day: 1 }, },
+    { title: "Pharisäer und Zöllner", year: { year: 1925, month: 6, day: 1 }, },
+    { title: "Die Bedingungen für das ewige Leben", year: { year: 1925, month: 6, day: 1 }, },
+    { title: "Die Furcht", year: { year: 1925, month: 6, day: 1 }, },
+    { title: "Im Anfang war", year: { year: 1925, month: 6, day: 1 }, },
+    { title: "Vernünftige Milch", year: { year: 1925, month: 6, day: 1 }, },
   ],
   Morgenwort: [
-    {title: "Vernünftige Milch", year: { year: 1925, month: 6, day: 1 },},
+    { title: "Vernünftige Milch", year: { year: 1925, month: 6, day: 1 }, },
   ],
   "Rila Vorträge": [
-    { title: "Pride and Prejudice", year: { year: 1926, month: 7, day: 3 } },
-    { title: "Outlander", year: { year: 1927, month: 8, day: 5 } },
-    { title: "The Notebook", year: { year: 1928, month: 9, day: 7 } },
-    { title: "Pride and Prejudice", year: { year: 1929, month: 10, day: 9 } },
-    { title: "Outlander", year: { year: 1930, month: 11, day: 11 } },
-    { title: "The Notebook", year: { year: 1931, month: 12, day: 13 } },
+    { title: "In Vorbereitung ...", year: { year: 1926, month: 7, day: 3 } },
   ],
   Jugendokkultklasse: [
-    { title: "Wolf Hall", year: { year: 1932, month: 1, day: 15 } },
-    { title: "The Book Thief", year: { year: 1933, month: 2, day: 17 } },
-    {
-      title: "All the Light We Cannot See",
-      year: { year: 1934, month: 3, day: 19 },
-    },
-    { title: "Wolf Hall", year: { year: 1935, month: 4, day: 21 } },
-    { title: "The Book Thief", year: { year: 1936, month: 5, day: 23 } },
-    {
-      title: "All the Light We Cannot See",
-      year: { year: 1937, month: 6, day: 25 },
-    },
+    { title: "In Vorbereitung ...", year: { year: 1932, month: 1, day: 15 } },
+
   ],
   "Allgemeine Okkultklasse": [
-    { title: "Normal People", year: { year: 1938, month: 7, day: 27 } },
-    { title: "The Midnight Library", year: { year: 1939, month: 8, day: 29 } },
-    {
-      title: "Beautiful World, Where Are You",
-      year: { year: 1940, month: 9, day: 1 },
-    },
-    { title: "Normal People", year: { year: 1941, month: 10, day: 3 } },
-    { title: "The Midnight Library", year: { year: 1942, month: 11, day: 5 } },
-    {
-      title: "Beautiful World, Where Are You",
-      year: { year: 1943, month: 12, day: 7 },
-    },
+    { title: "In Vorbereitung ...", year: { year: 1938, month: 7, day: 27 } },
+
   ],
   Zusammenkunftsvorträge: [
-    { title: "Beyond Good and Evil", year: { year: 1944, month: 1, day: 9 } },
-    { title: "The Republic", year: { year: 1945, month: 2, day: 11 } },
-    { title: "Meditations", year: { year: 1946, month: 3, day: 13 } },
-    {
-      title: "Thus Spoke Zarathustra",
-      year: { year: 1947, month: 4, day: 15 },
-    },
-    {
-      title: "Critique of Pure Reason",
-      year: { year: 1948, month: 5, day: 17 },
-    },
-    { title: "Being and Time", year: { year: 1949, month: 6, day: 19 } },
+    { title: "In Vorbereitung ...", year: { year: 1944, month: 1, day: 9 } },
   ],
   "Außerlplanmäßige Vorträge": [
     {
-      title: "Thinking, Fast and Slow",
+      title: "In Vorbereitung ...",
       year: { year: 1950, month: 7, day: 21 },
     },
-    {
-      title: "Man's Search for Meaning",
-      year: { year: 1951, month: 8, day: 23 },
-    },
-    { title: "The Power of Habit", year: { year: 1952, month: 9, day: 25 } },
-    {
-      title: "Quiet: The Power of Introverts",
-      year: { year: 1953, month: 10, day: 27 },
-    },
-    {
-      title: "Flow: of Optimal Experience",
-      year: { year: 1954, month: 11, day: 29 },
-    },
-    {
-      title: "Mindset: The New of Success",
-      year: { year: 1955, month: 12, day: 1 },
-    },
+
   ],
   "Letztes Wort": [
-    { title: "Good to Great", year: { year: 1956, month: 1, day: 3 } },
-    { title: "Zero to One", year: { year: 1957, month: 2, day: 5 } },
-    { title: "The Lean Startup", year: { year: 1958, month: 3, day: 7 } },
-    { title: "Start with Why", year: { year: 1959, month: 4, day: 9 } },
-    { title: "Deep Work", year: { year: 1960, month: 5, day: 11 } },
-    { title: "Atomic Habits", year: { year: 1961, month: 6, day: 13 } },
+    { title: "In Vorbereitung ...", year: { year: 1956, month: 1, day: 3 } },
   ],
   Jugenzusammenkünfte: [
-    { title: "Steve Jobs", year: { year: 1962, month: 7, day: 15 } },
-    { title: "Long Walk to Freedom", year: { year: 1963, month: 8, day: 17 } },
-    {
-      title: "The Diary of a Young Girl",
-      year: { year: 1964, month: 9, day: 19 },
-    },
-    { title: "Becoming", year: { year: 1965, month: 10, day: 21 } },
-    {
-      title: "Einstein: His Life and Universe",
-      year: { year: 1966, month: 11, day: 23 },
-    },
-    { title: "The Last Lion", year: { year: 1967, month: 12, day: 25 } },
+    { title: "In Vorbereitung ...", year: { year: 1962, month: 7, day: 15 } },
   ],
   "Klasse Tugenden": [
     {
-      title: "A Brief History of Time",
+      title: "In Vorbereitung ...",
       year: { year: 1968, month: 1, day: 27 },
     },
-    { title: "The Selfish Gene", year: { year: 1969, month: 2, day: 1 } },
-    { title: "Cosmos", year: { year: 1970, month: 3, day: 3 } },
-    { title: "The Origin of Species", year: { year: 1971, month: 4, day: 5 } },
-    { title: "Silent Spring", year: { year: 1972, month: 5, day: 7 } },
-    { title: "The Double Helix", year: { year: 1973, month: 6, day: 9 } },
   ],
   "Vorträge vor den Schwestern": [
-    { title: "The Innovators", year: { year: 1974, month: 7, day: 11 } },
-    { title: "Life 3.0", year: { year: 1975, month: 8, day: 13 } },
-    { title: "Superintelligence", year: { year: 1976, month: 9, day: 15 } },
-    { title: "The Code Book", year: { year: 1977, month: 10, day: 17 } },
-    {
-      title: "Artificial Intelligence",
-      year: { year: 1978, month: 11, day: 19 },
-    },
+    { title: "In Vorbereitung ...", year: { year: 1914, month: 7, day: 11 } },
+
   ],
   "Vorträge von den Leitern": [
     {
-      title: "The Art of Leadership",
-      year: { year: 1979, month: 12, day: 21 },
+      title: "In Vorbereitung ...",
+      year: { year: 1914, month: 12, day: 21 },
     },
-    { title: "Leading with Purpose", year: { year: 1980, month: 1, day: 23 } },
-    { title: "Spiritual Leadership", year: { year: 1981, month: 2, day: 25 } },
-    { title: "The Way of Wisdom", year: { year: 1982, month: 3, day: 27 } },
-    { title: "Sacred Knowledge", year: { year: 1983, month: 4, day: 29 } },
   ],
 };
 
